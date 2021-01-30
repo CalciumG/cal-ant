27771b0a23591d22e5031ad113f2a7349dba809c
Callum <callumgo@outlook.com>
2021-01-30 21:06:36 +0000

    Init commit

---
+++ src/components/Pricing.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="pricingBody">
+    <h1>{{ msg }}</h1>
+    <br />
+    <!-- Switch -->
+    <a-row justify="center">
+      <a-col :xs="{ span: 2 }" :lg="{ span: 1 }">
+        Monthly
+      </a-col>
+      <a-col :xs="{ span: 10 }" :lg="{ span: 1 }">
+        <div>
+          <a-switch default-checked @change="setPriceTerm" />
+        </div>
+      </a-col>
+      <a-col :xs="{ span: 2 }" :lg="{ span: 1 }">
+        Annually
+      </a-col>
+    </a-row>
+
+    <br />
+    <!-- Cards -->
+
+    <div style=" padding: 20px;">
+      <a-row justify="center">
+        <a-col :xs="{ span: 20 }" :lg="{ span: 5 }" class="taller">
+          <a-card>
+            <h1>Basic</h1>
+            <h1 class="price"><span>$</span>19.99</h1>
+            <div class="card-text">
+              <hr />
+              <p>card content</p>
+              <hr />
+              <p>card content</p>
+              <hr />
+              <p>card content</p>
+              <hr />
+            </div>
+          </a-card>
+        </a-col>
+        <a-col :xs="{ span: 20 }" :lg="{ span: 5 }" class="smaller">
+          <a-card>
+            <br />
+            <h1>Professional</h1>
+            <h1 class="price"><span>$</span>24.99</h1>
+            <div class="card-text">
+              <hr />
+              <p>card content</p>
+              <hr />
+              <p>card content</p>
+              <hr />
+              <p>card content</p>
+              <hr />
+            </div>
+            <br />
+          </a-card>
+        </a-col>
+        <a-col :xs="{ span: 20 }" :lg="{ span: 5 }" class="taller">
+          <a-card>
+            <h1>Master</h1>
+            <h1 class="price"><span>$</span>39.99</h1>
+            <div class="card-text">
+              <hr />
+              <p>card content</p>
+              <hr />
+              <p>card content</p>
+              <hr />
+              <p>card content</p>
+              <hr />
+            </div>
+          </a-card>
+        </a-col>
+      </a-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pricing",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    setPriceTerm(checked) {
+      var pricePeriod = checked ? "Annually" : "Monthly";
+      console.log(`Pricing set to: ${pricePeriod}`);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,39 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import {
+  Layout,
+  Button,
+  Spin,
+  Result,
+  Card,
+  Divider,
+  Col,
+  Row,
+  Drawer,
+  Table,
+  Form,
+  InputNumber,
+  Tag,
+  Switch,
+} from "ant-design-vue";
+import App from "./App.vue";
+import "./assets/css/style.css";
+import "./assets/bg-top.svg";
+import "./assets/bg-bottom.svg";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.config.productionTip = false;
+app.use(Layout);
+app.use(Button);
+app.use(Spin);
+app.use(Result);
+app.use(Card);
+app.use(Divider);
+app.use(Col);
+app.use(Row);
+app.use(Drawer);
+app.use(Table);
+app.use(Form);
+app.use(InputNumber);
+app.use(Tag);
+app.use(Switch);
+app.mount("#app");
