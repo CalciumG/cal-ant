0777f03ebdc7fc2aee295429faa5cf9a848f4796
Callum <callumgo@outlook.com>
2021-01-31 02:51:49 +0000

    added config for theming

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,11 @@ module.exports = {
     loaderOptions: {
       less: {
         lessOptions: {
+          modifyVars: {
+            "primary-color": "#af8ad8",
+            "link-color": "#1DA57A",
+            "border-radius-base": "2px",
+          },
           javascriptEnabled: true,
         },
       },
