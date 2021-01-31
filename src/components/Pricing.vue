49d0c3567121e69169ccd42509b1b3027cb2f32e
Callum <callumgo@outlook.com>
2021-01-31 23:24:56 +0000

    some basic price change

--- src/components/Pricing.vue
+++ src/components/Pricing.vue
@@ -22,10 +22,10 @@
 
     <div style=" padding: 20px;">
       <a-row justify="center">
-        <a-col :xs="{ span: 20 }" :lg="{ span: 5 }" class="taller">
+        <a-col :xs="{ span: 22 }" :lg="{ span: 5 }" class="taller">
           <a-card>
             <h1>Basic</h1>
-            <h1 class="price"><span>$</span>19.99</h1>
+            <h1 class="price"><span>$</span>{{ basicPrice }}</h1>
             <div class="card-text">
               <hr />
               <p>500 GB Storage</p>
@@ -39,11 +39,11 @@
             </div>
           </a-card>
         </a-col>
-        <a-col :xs="{ span: 20 }" :lg="{ span: 5 }" class="smaller">
+        <a-col :xs="{ span: 22 }" :lg="{ span: 5 }" class="smaller">
           <a-card>
             <br />
             <h1>Professional</h1>
-            <h1 class="price"><span>$</span>24.99</h1>
+            <h1 class="price"><span>$</span>{{ profPrice }}</h1>
             <div class="card-text">
               <hr />
               <p>1 TB Storage</p>
@@ -58,10 +58,10 @@
             <br />
           </a-card>
         </a-col>
-        <a-col :xs="{ span: 20 }" :lg="{ span: 5 }" class="taller">
+        <a-col :xs="{ span: 22 }" :lg="{ span: 5 }" class="taller">
           <a-card>
             <h1>Master</h1>
-            <h1 class="price"><span>$</span>39.99</h1>
+            <h1 class="price"><span>$</span>{{ masterPrice }}</h1>
             <div class="card-text">
               <hr />
               <p>2 TB Storage</p>
@@ -84,12 +84,28 @@
 export default {
   props: {},
   data() {
-    return {};
+    return {
+      basicPrice: 19.99,
+      profPrice: 24.99,
+      masterPrice: 39.99,
+    };
   },
   methods: {
-    setPriceTerm(checked) {
+    setPriceTerm(checked, basicPrice) {
       var pricePeriod = checked ? "Annually" : "Monthly";
-      console.log(`Pricing set to: ${pricePeriod}`);
+      this.basicPrice = checked ? 108.99 : 19.99;
+      console.log(`Pricing set to: ${pricePeriod} ${basicPrice}`);
+      this.setProfPriceTerm(checked);
+      this.setMasterPriceTerm(checked);
+      return basicPrice;
+    },
+    setProfPriceTerm(checked, profPrice) {
+      this.profPrice = checked ? 154.99 : 24.99;
+      return profPrice;
+    },
+    setMasterPriceTerm(checked, masterPrice) {
+      this.masterPrice = checked ? 204.99 : 39.99;
+      return masterPrice;
     },
   },
 };
